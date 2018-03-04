bc8a5ce4eb7a966cd5c3e5f4e293e953029f5304
Elisabethr <elisabethraid@gmail.com>
2018-03-04 19:51:26 +0200

    First commit

---
+++ webStoreProject/src/main/java/com/example/webStoreProject/WebStoreProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.webStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreProjectApplication.class, args);
+	}
+}
